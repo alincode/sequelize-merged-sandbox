0133648f2d3e3560c30366cc0f53ad53128a61f6
alincode <alin.code@gmail.com>
2016-07-24 17:06:02 +0800

    fixed typo

--- migrations/20160724084038-unnamed-migration.js
+++ migrations/20160724084038-unnamed-migration.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    queryInterface.addColumn(
+    return queryInterface.addColumn(
       'Users',
       'nick_name',
       Sequelize.STRING
